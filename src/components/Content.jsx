cb4f250fe0e3073db15429565444e68b86391667
gunysunny <mikan31@naver.com>
2025-06-02 11:03:31 +0900

    12일차 과제

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,10 @@
 export default function Content({ content }) {
   return (
-    <div className="content-container">
-      <img src={content.img} alt={content.title} />
-      <span>모집중</span>
-      <div>{content.title}</div>
-      <p>{content.subtitle}</p>
+    <div className="content-container flex flex-col items-start gap-[5px]">
+      <img src={content.img} alt={content.title} className="w-[300px] rounded-[10px] mb-[3px]" />
+      <span className="text-xs text-[#d7fa00] outline border-solid border-[#d7fa00] px-[5px] py-[4px] rounded-[3px]">모집중</span>
+      <div className="text-lg font-semibold">{content.title}</div>
+      <p className="text-xs text-[rgb(160,160,160)]">{content.subtitle}</p>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
-    <header className="header-container">
+    <header className="header-container flex justify-center content-center py-[10px] px-[20px] bg-black">
       <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+      <ul className="menu-container flex justify-center gap-[20px] items-center">
+        <li className="list-none font-normal text-[13px]">로그인</li>
+        <li className="list-none font-normal text-[13px]">회원가입</li>
+        <li className="list-none font-normal text-[13px]">내클래스</li>
       </ul>
     </header>
   );
